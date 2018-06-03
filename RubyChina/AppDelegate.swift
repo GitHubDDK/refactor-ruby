b0b6f53df2c4f3e4184aba25b3d39a5930f7beb7
Chandler <624586846@qq.com>
2018-06-03 14:15:22 +0800

    添加coredata

--- RubyChina/AppDelegate.swift
+++ RubyChina/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import AFNetworking
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -43,6 +44,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         case "compose":
             splitViewController.showDetailViewController(UINavigationController(rootViewController: ComposeController()), sender: nil)
         default: Void()
+        }
+    }
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "hangge_1841")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                
+                
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
         }
     }
 }
